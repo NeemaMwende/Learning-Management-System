59f45c2fe0fc31766fc2ed5b3e21efc3b02a70c6
Neema <neemamwende009@gmail.com>
2024-10-08 21:31:07 +0300

    backend urls.py updates

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@
 URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +15,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Desphixs LMS Backend APIs",
+      default_version='v1',
+      description="This is the API documentation for Desphixs LMS project APIs",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="destiny@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+     
     path('admin/', admin.site.urls),
+    # Add something new here
+
+    path("api/v1/", include("api.urls"))
 ]
 
-urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns  += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings. STATIC_URL, document_root=settings.STATIC_ROOT)
