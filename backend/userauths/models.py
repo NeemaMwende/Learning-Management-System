89779e0e09530adb2b36a28d00154203d1d6c201
Neema <neemamwende009@gmail.com>
2024-10-08 18:35:19 +0300

    updates

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, blank=True, null=True)  # Removed unique constraint
+    otp = models.CharField(max_length=100, null=True, blank=True)
+    refresh_token = models.CharField(max_length=1000, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        email_username, full_name = self.email.split("@")
+        if self.full_name == "" or self.full_name == None:
+            self.full_name == email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username
+        super(User, self).save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name if self.full_name else self.user.full_name
+    
+    def save(self, *args, **kwargs):
+        if not self.full_name:  # Corrected condition check
+            self.full_name = self.user.username
+        super(Profile, self).save(*args, **kwargs)
+
+
+# Signals for creating and saving Profile on User creation
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# Connecting signals
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
